8afdc8328588661a624bb6756b142acfa2106cbb
jacky-ennoconn <161289364+jacky-ennoconn@users.noreply.github.com>
2024-02-27 16:42:28 +0800

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,16 +1,17 @@
 # -*- coding: UTF-8 -*-
 import pickle
 import gzip
-import xgboost
+# import xgboost
 
 # 載入Model
 # with gzip.open('app/model/xgboost-iris.pgz', 'r') as f:
 #     xgboostModel = pickle.load(f)
-xgboostModel = xgb.XGBRegressor()
-xgboostModel.load_model('app/model/xgboost-iris.pgz')
+# xgboostModel = xgb.XGBRegressor()
+# xgboostModel.load_model('app/model/xgboost-iris.pgz')
 
 
 def predict(input):
-    pred=xgboostModel.predict(input)[0]
-    print(pred)
-    return pred
+    # pred=xgboostModel.predict(input)[0]
+    # print(pred)
+    return input
+    # return pred
