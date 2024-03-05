206aa8f013a5eedf5b9b967b540e96e5a93dec56
jacky-ennoconn <161289364+jacky-ennoconn@users.noreply.github.com>
2024-03-05 11:42:29 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,30 +12,27 @@ CORS(app)
 def getResult():
     input_array = np.array([[5.5, 2.4, 2.7, 1.]])
     result = np.sum(input_array)
-    # result = model.predict(input)
     response = app.response_class(
         response=jsonify({'result': str(result)}),
         status=200,
         mimetype='application/json'
     )
-    return response
-    # return "getResult"
+    return [response]  # 返回可迭代對象
 
 @app.route('/predict', methods=['POST'])
 def postInput():
     # 取得前端傳過來的數值
     insertValues = request.get_json()
-    x1=insertValues['sepalLengthCm']
-    x2=insertValues['sepalWidthCm']
-    x3=insertValues['petalLengthCm']
-    x4=insertValues['petalWidthCm']
+    x1 = insertValues['sepalLengthCm']
+    x2 = insertValues['sepalWidthCm']
+    x3 = insertValues['petalLengthCm']
+    x4 = insertValues['petalWidthCm']
     input_array = np.array([[x1, x2, x3, x4]])
     # 進行預測
-    # result = model.predict(input)
     result = np.sum(input_array)
     response = app.response_class(
         response=jsonify({'result': str(result)}),
         status=200,
         mimetype='application/json'
     )
-    return response
+    return [response]  # 返回可迭代對象
